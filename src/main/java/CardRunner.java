c52569dcee0e3926c1fd757d79aa7dba3eedb6dc
Adah Harding <adahharding@gmail.com>
2020-10-16 23:09:27 +0000

    First attempt

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,24 @@
 public class CardRunner {
     public static void main(String args[]){
         // ☕
-        System.out.println("hey");
+        //System.out.println("hey");
+        TerrapinCard billyCard = new TerrapinCard(20);
+        TerrapinCard brianCard = new TerrapinCard(30);
+        
+        billyCard.payGourmet();
+        System.out.println("Billy: " + billyCard);
+        brianCard.payEconomical();
+        System.out.println("Brian: " + brianCard);
 
+        billyCard.loadMoney(20);
+        System.out.println("Billy: " + billyCard);
+        brianCard.payGourmet();
+        System.out.println("Brian: " + brianCard);
+
+        billyCard.payEconomical();
+        billyCard.payEconomical();
+        System.out.println("Billy: " + billyCard);
+        brianCard.loadMoney(50);
+        System.out.println("Brian: " + brianCard);
     }
 }
